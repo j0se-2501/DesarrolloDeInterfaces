f24d51ed04835f68b9a2da83dba9fdc393a3b059
j0se-2501 <jolufer23@gmail.com>
2023-10-09 15:13:33 +0200

    commit desde clase

--- AplicacionTroll/src/troleo/Principal.java
+++ AplicacionTroll/src/troleo/Principal.java
@@ -1,21 +1,10 @@
 package troleo;
 
 import java.awt.EventQueue;
-import java.io.File;
-
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
 
 public class Principal extends Inicio{
 	
 	
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
